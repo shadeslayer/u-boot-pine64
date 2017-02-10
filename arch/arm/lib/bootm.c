bc29b1829bf391a3552640c35b08d70cede8d156
Kamil Trzcinski <ayufan@ayufan.eu>
2017-02-10 22:29:41 +0100

    Make it compile (again)

--- arch/arm/lib/bootm.c
+++ arch/arm/lib/bootm.c
@@ -64,7 +64,7 @@ void arch_lmb_reserve(struct lmb *lmb)
 		    gd->bd->bi_dram[0].start + gd->bd->bi_dram[0].size - sp);
 }
 
-#if 0
+#if 1
 /**
  * announce_and_cleanup() - Print message and prepare for kernel boot
  *
